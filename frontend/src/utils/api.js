c1ba2b4ad811fdae02d6aba454e61ec31fc642ee
krishnashukla1 <120076833+krishnashukla1@users.noreply.github.com>
2025-08-16 06:53:41 +0530

    first commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api", // adjust to match your backend
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
+//-------------------------
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // adjust to match your backend
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // ✅ dynamic backend URL
 });
 
 API.interceptors.request.use((config) => {
