73b9ba314fd7eb24beaee7883898618048c93092
krishnashukla1 <120076833+krishnashukla1@users.noreply.github.com>
2025-08-16 07:29:04 +0530

    first commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -19,7 +19,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`, // ✅ dynamic backend URL
-  // baseURL: `${import.meta.env.VITE_API_URL}`, // ✅ dynamic backend URL
+
 
 });
 
